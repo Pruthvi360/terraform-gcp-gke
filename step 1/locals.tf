70dca27ee3118f3853e45d85864d22889683f7c6
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2022-11-30 12:41:47 +0530

    Update locals.tf

--- step 1/locals.tf
+++ step 1/locals.tf
@@ -1,7 +1,7 @@
 locals {
   region               = "us-west2"
-  org_id               = "772284654262"
-  billing_account      = "01B96A-7D7A26-AF0E9A"
+  org_id               = "**"
+  billing_account      = "**"
   host_project_name    = "automation-kube"
   service_project_name = "kube-pruthvi"
   host_project_id      = "${local.host_project_name}-${random_integer.int.result}"
